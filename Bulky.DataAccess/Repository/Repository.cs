2617181aab0395bfcf941d33b299dd5c62522618
yuadit <yuaditdev@gmail.com>
2024-07-24 14:38:16 +0700

    Implementation Include to show CategoryName in Product Table View

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -9,23 +9,41 @@ public class Repository<T> : IRepository<T> where T : class
 {
     private readonly ApplicationDbContext _db;
     internal DbSet<T> dbSet;
+    private static readonly char[] separator = new char[]{','};
 
     public Repository(ApplicationDbContext db)
     {
         _db = db;
         dbSet = _db.Set<T>();
+        // _db.Products.Include(u => u.Category).Include(u => u.CategoryId);
     }
 
-    public IEnumerable<T> GetAll()
+    public IEnumerable<T> GetAll(string? includeProperties = null)
     {
         IQueryable<T> query = dbSet;
+        if (!string.IsNullOrEmpty(includeProperties))
+        {
+            foreach (var includeProperty in includeProperties.Split(separator, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query
+                    .Include(includeProperty);
+            }
+        }
         return query.ToList();
     }
 
-    public T Get(Expression<Func<T, bool>> filter)
+    public T Get(Expression<Func<T, bool>> filter,string? includeProperties = null)
     {
         IQueryable<T> query = dbSet;
         query = query.Where(filter);
+        if (!string.IsNullOrEmpty(includeProperties))
+        {
+            foreach (var includeProperty in includeProperties.Split(separator, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query
+                    .Include(includeProperty);
+            }
+        }
         return query.FirstOrDefault() ?? throw new InvalidOperationException();
     }
 
--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -20,13 +20,13 @@ public class ProductController : Controller
 
     public ActionResult Index()
     {
-        var objProductList = _unitOfWork.Product.GetAll().ToList();
+        var objProductList = _unitOfWork.Product.GetAll("Category").ToList();
         return View(objProductList);
     }
 
     public IActionResult Upsert(int? id)
     {
-        IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+        var CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
         {
             Text = u.Name,
             Value = u.Id.ToString()
@@ -38,17 +38,11 @@ public class ProductController : Controller
             CategoryList = CategoryList,
             Product = new Product()
         };
-        if (id == null || id == 0)
-        {
-            return View(productVm);
-        }
-        else
-        {
-            //update
-            productVm.Product = _unitOfWork.Product.Get(u => u.Id == id);
-            return View(productVm);
-        }
-        
+        if (id == null || id == 0) return View(productVm);
+
+        //update
+        productVm.Product = _unitOfWork.Product.Get(u => u.Id == id);
+        return View(productVm);
     }
 
     [HttpPost]
@@ -65,43 +59,37 @@ public class ProductController : Controller
             });
             return View(productVM);
         }
-        
-        string wwwRootPath = _webHostEnvironment.WebRootPath;
+
+        var wwwRootPath = _webHostEnvironment.WebRootPath;
         if (file != null)
         {
-            string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-            string productPath = Path.Combine(wwwRootPath, @"images\product");
+            var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+            var productPath = Path.Combine(wwwRootPath, @"images\product");
 
             if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
             {
                 //delete the old image
                 var oldImagePath = Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
-                if (System.IO.File.Exists(oldImagePath))
-                {
-                    System.IO.File.Delete(oldImagePath);
-                }
+                if (System.IO.File.Exists(oldImagePath)) System.IO.File.Delete(oldImagePath);
             }
-            
-            using (var fileStream = new FileStream(Path.Combine(productPath,fileName),FileMode.Create))
+
+            using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
             {
                 file.CopyTo(fileStream);
             }
+
             productVM.Product.ImageUrl = @"\images\product\" + fileName;
         }
         else
         {
-            productVM.Product.ImageUrl = "";
+            if (productVM.Product.Id == 0) productVM.Product.ImageUrl = "";
         }
 
         if (productVM.Product.Id == 0)
-        {
             _unitOfWork.Product.Add(productVM.Product);
-        }
         else
-        {
             _unitOfWork.Product.Update(productVM.Product);
-        }
-        
+
         _unitOfWork.Save();
         TempData["success"] = "Product created successfully";
         return RedirectToAction("Index");
