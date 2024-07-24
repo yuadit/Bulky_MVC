441886da030d4073267d4564f0f7073f7813ae42
yuadit <yuaditdev@gmail.com>
2024-07-24 10:42:05 +0700

    Add & Seed Product Table, Create Product Repository

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -6,12 +6,14 @@ namespace Bulky.DataAccess.Repository;
 public class UnitOfWork : IUnitOfWork
 {
     private readonly ApplicationDbContext _db;
-    public ICategoryRepository Category { get; private set; }
+    public ICategoryRepository Category { get; }
+    public IProductRepository Product { get; }
 
     public UnitOfWork(ApplicationDbContext db)
     {
         _db = db;
         Category = new CategoryRepository(_db);
+        Product = new ProductRepository(_db);
     }
     
     public void Save()
