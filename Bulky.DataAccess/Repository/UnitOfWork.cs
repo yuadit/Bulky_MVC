6803758b7fa220eab7c17c0248661f6eb1e8b344
yuadit <yuaditdev@gmail.com>
2024-07-28 14:46:36 +0700

    Add Application User to Repository

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -14,10 +14,12 @@ public class UnitOfWork : IUnitOfWork
         Product = new ProductRepository(_db);
         Company = new CompanyRepository(_db);
         ShoppingCart = new ShoppingCartRepository(_db);
+        ApplicationUser = new ApplicationUserRepository(_db);
     }
 
     public ICompanyRepository Company { get; }
     public IShoppingCartRepository ShoppingCart { get; }
+    public IApplicationUserRepository ApplicationUser { get; }
 
 
     public ICategoryRepository Category { get; }
