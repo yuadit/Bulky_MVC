3700856bf43cad4bfafd5d60170c578dc2d2b7b0
yuadit <yuaditdev@gmail.com>
2024-07-29 11:11:51 +0700

    Add Session Id in OderHeader Model

--- Bulky.Models/OrderHeader.cs
+++ Bulky.Models/OrderHeader.cs
@@ -25,6 +25,7 @@ public class OrderHeader
     public DateTime PaymentDate { get; set; }
     public DateOnly PaymentDueDate { get; set; }
 
+    public string? SessionId { get; set; }
     public string? PaymentIntentId { get; set; }
 
     [Required] public string PhoneNumber { get; set; }
