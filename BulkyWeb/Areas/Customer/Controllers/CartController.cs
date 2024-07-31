99761da681297728d847f4cbee4a9bd8c780df38
yuadit <yuaditdev@gmail.com>
2024-07-31 10:38:08 +0700

    Add Cart Count Logic in Home And Cart (When Remove Item)

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -210,6 +210,8 @@ public class CartController : Controller
         if (cartFromDb.Count <= 1)
         {
             //remove that from cart
+            HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
+                .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
             _unitOfWork.ShoppingCart.Remove(cartFromDb);
         }
         else
@@ -225,6 +227,8 @@ public class CartController : Controller
     public IActionResult Remove(int cartId)
     {
         var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+        HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
+            .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
         _unitOfWork.ShoppingCart.Remove(cartFromDb);
         _unitOfWork.Save();
         return RedirectToAction(nameof(Index));
