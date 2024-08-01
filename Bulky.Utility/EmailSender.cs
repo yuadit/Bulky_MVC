dc1ed392cf828bde84e192fad41979e080a5225d
yuadit <yuaditdev@gmail.com>
2024-08-01 10:55:23 +0700

    Add Brevo Email Implementation

--- Bulky.Utility/EmailSender.cs
+++ Bulky.Utility/EmailSender.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendWithBrevo;
 
 namespace Bulky.Utility;
 
 public class EmailSender : IEmailSender
 {
+    public string BravoSecret { get; set; }
+
+    public EmailSender(IConfiguration _config)
+    {
+        BravoSecret = _config.GetValue<string>("Bravo:SecretKey");
+    }
     public Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        return Task.CompletedTask;
+        var client = new BrevoClient(BravoSecret);
+        var newmail = client.SendAsync(
+            new Sender("Bulk Book", "hello@dotnetmastery.com"),
+            new List<Recipient> { new("Dear Customer", email) },
+            subject,
+            htmlMessage // true if body is HTML
+        );
+
+        return newmail;
     }
 }
