bf9fdf1635b526ef38b7b3a73b9277fcd279d6ff
yuadit <yuaditdev@gmail.com>
2024-08-02 10:42:32 +0700

    Fix Bug button Lock/Unlock not working

--- BulkyWeb/wwwroot/js/user.js
+++ BulkyWeb/wwwroot/js/user.js
@@ -22,10 +22,10 @@ function loadDataTable() {
                     if (lockout > today) {
                         return `
                         <div class="text-center">
-                             <a href="/admin/user/RoleManagment?userId=${data.id}" class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
+                             <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
                                     <i class="bi bi-lock-fill"></i>  Lock
                                 </a> 
-                                <a class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
+                                <a href="/admin/user/RoleManagment?userId=${data.id}" class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
                                      <i class="bi bi-pencil-square"></i> Permission
                                 </a>
                         </div>
@@ -33,10 +33,10 @@ function loadDataTable() {
                     } else {
                         return `
                         <div class="text-center">
-                              <a href="/admin/user/RoleManagment?userId=${data.id}" class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
+                               <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:100px;">
                                     <i class="bi bi-unlock-fill"></i>  UnLock
                                 </a>
-                                <a class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
+                                <a href="/admin/user/RoleManagment?userId=${data.id}" class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
                                      <i class="bi bi-pencil-square"></i> Permission
                                 </a>
                         </div>
