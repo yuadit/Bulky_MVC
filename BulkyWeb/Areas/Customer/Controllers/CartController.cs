8c3b17aeeef78f5a3ea67de278c506d0e4ff31d4
yuadit <yuaditdev@gmail.com>
2024-08-05 14:25:23 +0700

    Bug Solution & Logout (cntd.)

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -216,13 +216,13 @@ public class CartController : Controller
 
     public IActionResult Minus(int cartId)
     {
-        var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked: true);
+        var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
         if (cartFromDb.Count <= 1)
         {
             //remove that from cart
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
             HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
                 .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
-            _unitOfWork.ShoppingCart.Remove(cartFromDb);
         }
         else
         {
@@ -236,10 +236,11 @@ public class CartController : Controller
 
     public IActionResult Remove(int cartId)
     {
-        var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked: true);
+        var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+        _unitOfWork.ShoppingCart.Remove(cartFromDb);
         HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
             .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
-        _unitOfWork.ShoppingCart.Remove(cartFromDb);
+        
         _unitOfWork.Save();
         return RedirectToAction(nameof(Index));
     }
